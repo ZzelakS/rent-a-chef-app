54bbec8006bc10f51eefda83d5628e4ab6edcaa1
ZzelakS <adamsskalezz@gmail.com>
2024-01-26 23:21:23 +0100

    updated devDependency

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,84 +1,48 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import "../assets/styles/SignUp.css";
-import Add from "../assets/images/Add_Image_icon-icons.com_54218.png";
+// import Add from "../assets/images/Add_Image_icon-icons.com_54218.png";
+import Logo from '../assets/images/Logo.png'
 
 import user_icon from "../assets/images/image.png";
 import email_icon from "../assets/images/email.png";
 import password_icon from "../assets/images/password.png";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 import rectangle from "../assets/images/Rectangle 96.png";
 
 function SignUp() {
-  const [action, setAction] = useState("Sign Up");
 
   return (
-    <div>
-      <div>
-        <Navbar />
-      </div>
-      <div className="hero">
+    <div className="login">
+        <div className="hero">
         <img src={rectangle} alt="" />
       </div>
-      <div className="container">
-        <div className="header">
-          <div className="text">{/* {action} */}</div>
-          {/* <div className='underline'></div> */}
+        <div className="login-hero">
+        <img src={Logo} alt=''/>
         </div>
-        <div className="inputs">
-          {/* {action === "Login" ? (
-            <div></div>
-          ) : ( */}
-            <div className="input">
-              <img src={user_icon} alt="" />
-              <input type="text" placeholder="Name" />
-            </div>
-          
-          <div className="input">
-            <img src={email_icon} alt="" />
-            <input type="email" placeholder="Email ID" />
-          </div>
-          <div className="input">
-            <img src={password_icon} alt="" />
-            <input type="password" placeholder="Password" />
-          </div>
-          {/* {action === "Login" ? (
-            <div></div>
-          ) : ( */}
-            <div className="input">
-              <img src={password_icon} alt="" />
-              <input type="password" placeholder=" Confirm Password" />
-            </div>
-        </div>
-        {/* {action === "Sign Up" ? (
-          <div></div>
-        ) : ( */}
-          <div className="forgot-password">
-            Lost Password? <span>Click Here!</span>
-          </div>
-        {action === "Login" ? (
-          <div></div>
-        ) : (
-          <div>
-            <input className="p-pix" type="file" id="file" />
-            <label htmlFor="file" className="add">
-              <img src={Add} alt="" className="add" />
-              <span>Add an Avatar</span>
-            </label>
-          </div>
-        )}
-        <div className="submit-container">
-          <div
-            className={action === "Login" ? "submit gray" : "submit"}
-            onClick={() => {
-              setAction("Sign Up");
-            }}
-          >
-            Sign Up
-          </div>
-          
-        </div>
+      <div className="login-text">
+        <h3>Login to Your Kook Account</h3>
       </div>
+      <div className="input">
+        <img src={user_icon} alt="" />
+        <input type="email" placeholder="Name" />
+      </div>
+      <div className="input">
+        <img src={email_icon} alt="" />
+        <input type="email" placeholder="Email ID" />
+      </div>
+      <div className="input">
+        <img src={password_icon} alt="" />
+        <input type="password" placeholder="Password" />
+      </div>
+      <div className="input">
+        <img src={password_icon} alt="" />
+        <input type="password" placeholder="Confirm Password" />
+      </div>
+          <div className="submit" style={{fontFamily: 'Poppins'}}>Sign Up</div>
+      <div className="reset">
+            Already have an account? <span>Click Here!</span>
+          </div>
     </div>
   );
 }
